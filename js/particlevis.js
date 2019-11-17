6bbb6541710ac48c92027ee198073ad41254eb67
Guangyu Du <guangyudu@Guangyus-MacBook-Pro.local>
2019-11-17 18:57:58 -0500

    modify particle trail

--- js/particlevis.js
+++ js/particlevis.js
@@ -6,7 +6,6 @@ function particlevisMain(){
 	renderer.autoClearColor = false;   // do not clear buffer for rendering particle trails
 	renderer.setSize($('#dugy-c').parent().width(), $('#dugy-c').parent().height(), false);
 	var clearFlagColor = new THREE.Color( 0x161c26);
-	renderer.setClearColor(clearFlagColor,0);
 	
 	// set camera
 	//var camera = new THREE.OrthographicCamera(-1000,1000, 1000, -1000, 1,1000 );  
@@ -16,41 +15,62 @@ function particlevisMain(){
 	// scene
 	var scene = new THREE.Scene();
 	
+	// colors
+	var startColor = new THREE.Color(0xff6666);
+	var destColor = new THREE.Color(0x161c26);
+	// Draw a Plane in front of particles to erase old ones
+	// var veil_geo = new THREE.PlaneGeometry(20000,20000);
+	// var veil_mat = new THREE.MeshBasicMaterial( {color: 0x000000, opacity:0.012, transparent:true,blending: THREE.CustomBlending, side: THREE.DoubleSide} );
+	// veil_mat.blendEquation = THREE.ReverseSubtractEquation;
+	// veil_mat.blendSrc = THREE.SrcAlphaFactor;
+	// veil_mat.blendDst = THREE.OneFactor;
+	// var veil_obj = new THREE.Mesh(veil_geo, veil_mat);
+	// scene.add(veil_obj);
+	// veil_obj.position.z = -200;
+	// veil_obj.renderOrder = -1;
+	
 	
 	// Draw Particles
 	var numStars = 100;
-	var starsGeometry = new THREE.Geometry();
 	var starsBufferGeometry = new THREE.BufferGeometry();
 	
 	// attribute buffers
 	var starsPositionBuffer = new Float32Array( numStars * 3 );
+	var starsColorBuffer = new Float32Array( numStars * 3 );
 	var starsScaleBuffer = new Float32Array( numStars );
 	var starsObjects = new Array(numStars);
-	for ( var i = 0; i < numStars; i ++ ) {
 	
+	var originScale = 1;
+	for ( var i = 0; i < numStars; i ++ ) {
+		// positions
 		var star = new THREE.Vector3();
 		star.x = THREE.Math.randFloatSpread( 2000);
 		star.y = THREE.Math.randFloatSpread( 2000);
 		star.z = -100;
-		starsGeometry.vertices.push( star );
 	
 		starsPositionBuffer[i*3] = star.x;
 		starsPositionBuffer[i*3 + 1] = star.y;
 		starsPositionBuffer[i*3 + 2] = star.z;
 		starsScaleBuffer[i] = 1;
+		
+		var life = THREE.Math.randInt(36,720);
+		starsObjects[i] = new Star(star.x,star.y,star.z,life);
 	
-		starsObjects[i] = new Star(star.x,star.y,star.z);
+		
+		starsColorBuffer[i*3] = startColor.r;
+		starsColorBuffer[i*3+1] = startColor.g;
+		starsColorBuffer[i*3+2] = startColor.b;
+	
 	}
 	starsBufferGeometry.setAttribute( 'position', new THREE.BufferAttribute( starsPositionBuffer, 3 ) );
-	starsBufferGeometry.setAttribute( 'scale', new THREE.BufferAttribute( starsScaleBuffer, 1 ) );
+	starsBufferGeometry.setAttribute( 'size', new THREE.BufferAttribute( starsScaleBuffer, 1 ) );
+	starsBufferGeometry.setAttribute( 'color', new THREE.BufferAttribute( starsColorBuffer, 3) );
 	
 	
 	var starsTexture = new THREE.TextureLoader().load( "textures/TEX_Glow.png" );
 	var starsMaterial = new THREE.PointsMaterial( 
-		{ color: 0xff6666, map:starsTexture, blending: THREE.AdditiveBlending, size:2} );
+		{ vertexColors: THREE.VertexColors , map:starsTexture, blending: THREE.AdditiveBlending, depthTest: true,size:3} );
 	
-	// using Geometry as particle system
-	//var starField = new THREE.Points( starsGeometry, starsMaterial );
 	
 	// using Buffer Geometry as particle system
 	var starField = new THREE.Points( starsBufferGeometry, starsMaterial );
@@ -61,17 +81,32 @@ function particlevisMain(){
 	
 	function starFieldUpdate(){
 		var positions = starField.geometry.attributes.position.array;
-		var scales = starField.geometry.attributes.scale.array;
+		var colors = starField.geometry.attributes.color.array;
+		var scales = starField.geometry.attributes.size.array;
+	
 		for (var i = 0; i<numStars; i++){
-			starsObjects[i].wander();
-			starsObjects[i].update();
+			if(starsObjects[i].remain >= 1){
+	
+				starsObjects[i].wander();
+				starsObjects[i].update();
+			}
 			positions[i*3] = starsObjects[i].pos.x; 
 			positions[i*3+1] = starsObjects[i].pos.y; 
 			positions[i*3+2] = starsObjects[i].pos.z; 
+			
+			var color = startColor.clone();
+			color.lerp(destColor, 2 * Math.abs( starsObjects[i].alpha - 0.5));
+			colors[i*3] = color.r;
+			colors[i*3+1] = color.g;
+			colors[i*3+2] = color.b;
 	
-	
+			scales[i] =  2 * Math.abs(0.5 - starsObjects[i].alpha);
 		}
 		starField.geometry.attributes.position.needsUpdate = true;
+		starField.geometry.attributes.color.needsUpdate = true;
+		starField.geometry.attributes.size.needsUpdate = true;
+	
+	
 	}
 	
 	
--- js/star.js
+++ js/star.js
@@ -1,18 +1,30 @@
 // Star Class
 class Star{
 	pos = new THREE.Vector3(0,0,0);
-	vel = new THREE.Vector3(0,THREE.Math.randFloat(-0.5,0.5),0);
+	vel = new THREE.Vector3(0,THREE.Math.randFloat(-0.1,0.1),0);
 	acc = new THREE.Vector3(0,0,0);
-	wandertheta = 0;
-	static maxspeed = 1.5;
-	static maxforce = 0.05;
+    wandertheta = 0;
+    life = 0;
+    life2 = 0
+    lifetime = 1;
+	static maxspeed = 1.2;
+	static maxforce = 0.03;
 
-	constructor(_x,_y,_z){
+	constructor(_x,_y,_z,_life){
 	this.pos.x = _x;
 	this.pos.y = _y;
-	this.pos.z = _z;
+    this.pos.z = _z;
+    this.life = _life;
+    this.life2 = 2* _life;
+    this.lifetime = _life;
 	}
+    get alpha() {
+        return this.life/this.lifetime;
+      }
 
+    get remain(){
+        return this.life2;
+    }
     wander(){
         var wanderR = 2.5;
         var wanderD = 8;
@@ -38,7 +50,6 @@ class Star{
         target.add(circlepos);
 
         this.seek(target);
-    
     }
 
 	seek(target){
@@ -50,6 +61,14 @@ class Star{
 		desired.clampLength(0,Star.maxforce);
 		this.applyforce(desired)
 	}
+    decay(){
+        if (this.life > 0 ){
+            this.life -= 1;
+        }
+        if(this.life2 > 0){
+            this.life2 -=1;
+        }
+    }
 
 	applyforce(force){
 		this.acc.add(force);
@@ -58,7 +77,8 @@ class Star{
         this.vel.add(this.acc);
         this.vel.clampLength(0,Star.maxspeed);
 		this.pos.add(this.vel);
-		this.acc.multiplyScalar(0);
+        this.acc.multiplyScalar(0);
+        this.decay();
 	}
 }
 
