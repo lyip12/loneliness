4b5457296acb5f772bc07f15d748f9ed47c0ed84
Lucy Yip <lyip@risd.edu>
2019-11-18 17:05:39 -0500

    asdfghj

--- js/testscroll.js
+++ js/testscroll.js
@@ -2,7 +2,8 @@ sectiontest();
 function sectiontest(){
 
     var winheight = window.innerHeight || (document.documentElement || document.body).clientHeight;
-    
+
+    var scrollTop, init = 0, trigger = 0;
 
     function getDocHeight() {
         var D = document;
@@ -16,14 +17,39 @@ function sectiontest(){
     var docheight = getDocHeight();
 
     function amountscrolled(){
-        var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
-        console.log(scrollTop)
+        scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+        console.log(init);
+        console.log(winheight);
+        console.log(scrollTop);
     }
 
     window.addEventListener("scroll", function(){
         amountscrolled()
+        if(init !== 0 && scrollTop>winheight/2+init){
+            console.log("it is changing the top margin")
+            document.getElementById("theTarget2").style.paddingTop = 200+"px";
+        }
     }, false)
+
+    function isScrolledIntoView(elem)
+    {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+        return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    $(window).scroll(function() {    
+        if(isScrolledIntoView($('#theTarget')) && trigger == 0)
+        {
+            init = scrollTop;
+            trigger = 1;
+        }    
+    });
+
+
+
 
 
 }
-//document.getElementById("testerblock").style.marginTop = scrollTop+"px";
