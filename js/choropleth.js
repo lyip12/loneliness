9e11a595f16c3e568b039a1aa973e81ef9a511e6
Lucy Yip <lyip@risd.edu>
2019-11-25 00:29:35 -0500

    sdfs

--- js/choropleth.js
+++ js/choropleth.js
@@ -2,7 +2,6 @@ demographicschorovis() // for avoiding naming issue
 
 function demographicschorovis(){
     var toggle;
-    var cate = "minority"
     //responsive layout from https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
     var svg = d3.select("#yipchoro")
     .classed("yipsvg-container", true)
@@ -28,9 +27,12 @@ function demographicschorovis(){
 
     //actual chorovis starts
     function updateChoropleth() {
-        
+
         var selector = d3.select("#yipstats").property("value");
-        var selector2 = d3.select("#yipstats").property("value2");
+        //var selector2 = d3.select("#yipstats").property("value2");
+
+        if(selector == "One_Person_Household_Frequent"){ var cate = "one person household"; } else if(selector == "Mutiple_People_Household_Frequent"){ var cate = "multiple people household"; } else if(selector == "Conflict_Never_Frequent"){ var cate = "no conflict household"; } else if(selector == "Conflict_Sometimes_Frequent"){ var cate = "sometimes conflict household"; } else if(selector == "Conflict_Often_Frequent"){ var cate = "often conflicting household"; } else if(selector == "No_Confidant_Frequent"){ var cate = "no confidant"; } else if(selector == "At_Least_One_Confidant_Frequent"){ var cate = "more than one confidant"; } else if(selector == "Native_Frequent"){ var cate = "native (local)"; } else if(selector == "Immigrant_Frequent"){ var cate = "immigrant"; } else if(selector == "Ethnic_Minority_Frequent"){ var cate = "ethinic minority"; } else if(selector == "Ethnic_Majority_Frequent"){ var cate = "ethnic majority"; } else if(selector == "Religious_Frequent"){ var cate = "religious"; } else if(selector == "Non_Religious_Frequent"){ var cate = "non-religious"; } else if(selector == "Big_City_Frequent"){ var cate = "big city"; } else if(selector == "Suburb_Frequent"){ var cate = "suburb"; } else if(selector == "Small_City_Frequent"){ var cate = "small city"; } else if(selector == "Countryside_Frequent"){ var cate = "countryside"; } else if(selector == "Unemployed_Past_Year_Frequent"){ var cate = "past year unemployed"; } else if(selector == "Unemployed_Past_Month_Frequent"){ var cate = "past month unemployed"; } else if(selector == "Unemployed_Past_Days_Frequent"){ var cate = "past days unemployed"; } else if(selector == "Employed_Frequent"){ var cate = "employed"; } else if(selector == "Income_Comfortable_Frequent"){ var cate = "comfortable income"; } else if(selector == "Income_Coping_Frequent"){ var cate = "coping income"; } else if(selector == "Income_Difficult_Frequent"){ var cate = "difficult income"; } else if(selector == "Income_Very_Difficult_Frequent"){ var cate = "very difficult income"; } else { var cate = "all"; };
+
         a = [];
         //console.log(selector2);
         d3.queue()
@@ -90,8 +92,8 @@ function demographicschorovis(){
                     Tooltip
                         .style("opacity", 0)
 
-//                    var yipchoroselector = "All";
-//                    yipsmallmultiples(yipchoroselector);
+                    var yipchoroselector = "All";
+                    yipsmallmultiples(yipchoroselector);
 
                     toggle =1;
                     var t = " "
@@ -107,8 +109,7 @@ function demographicschorovis(){
                             .transition()
                             .duration(800)
                             .style("opacity", 1)
-                        console.log(d)
-                        tip = "in " + d.properties.name + ", around " + d.data + "% of " + cate + " population is lonely.";
+                        tip = "in " + d.properties.name + ", around " + d.data + "% of " + cate + " population is frequently lonely.";
 
                         Tooltip
                             .html(tip)
@@ -118,8 +119,8 @@ function demographicschorovis(){
                             .duration(800)
                             .style("opacity", 1);
 
-//                        var yipchoroselector = d.properties.name;
-//                        yipsmallmultiples(yipchoroselector);
+                        var yipchoroselector = d.properties.name;
+                        yipsmallmultiples(yipchoroselector);
 
                         var t = d.data;
                         document.getElementById("yiptooltip").innerHTML = t;
@@ -141,8 +142,8 @@ function demographicschorovis(){
 
             for(var i = 0; i<9;i++){
                 svg.append("rect")
-                    .attr("x", i*7+50)
-                    .attr("y", 0)
+                    .attr("x", 80)
+                    .attr("y", 64-i*7)
                     .attr("width", 7)
                     .attr("height", 7)
                     .transition()
@@ -150,6 +151,28 @@ function demographicschorovis(){
                     .attr("fill", function(d){
                     return fill(d3.min(a)+k*i);
                 }); 
+
+                var num = (d3.min(a)+k*i);
+                var n = num.toFixed(2);
+
+                svg.append("text")
+                    .attr("x", 90)
+                    .attr("y", 69-i*7)
+                    .attr("font-family", "'Roboto', sans-serif")
+                    .attr("font-size", "5px")
+                    .attr("fill", "#8293b6")
+                    .text(n + "%");
+                
+                if(i=0){
+                    
+                    svg.append("text")
+                        .attr("x", 90)
+                        .attr("y", 72)
+                        .attr("font-family", "'Roboto', sans-serif")
+                        .attr("font-size", "5px")
+                        .attr("fill", "#8293b6")
+                        .text("0%");
+                }
             }
 
 
